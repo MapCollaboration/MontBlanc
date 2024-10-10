58bc5454ce8a6fee5acbcde7c39783942eedf9d5
Canzian <canzian21@gmail.com>
2024-10-10 16:30:03 +0200

    Added dockerfile and script for Optimize

---
+++ transfer/Optimize/script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH -p standard
+#SBATCH --chdir=/home/lordguto/Montblanc/build/run
+
+## generation of Optimize command one for each replica
+
+jbegin=1
+jend=6
+nrep=250
+cpus=6
+mode=${3:-'f1D1'}
+for i in #insert nodenames 
+ do
+    for (( r=$jbegin; r <= $jend; r++ ))
+     do
+        occam-run -n $i -s something Optimize ./Optimize $r ../../config/MAPFF20/PI/MAPFF20_PI_NNLO_Q1_00.yaml ../../data/ fit/
+        if [$r -eq $nrep]
+        then
+          break
+        fi
+     done
+     if [$r -eq $nrep]
+     then
+        break
+     fi
+     jbegin=$(($jend+1))
+     jend=$(($jbegin+5))
+ done
