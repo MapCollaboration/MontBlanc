fd8ab0c4d160e4b4e27417f2ff02c9529b383b63
achiefa <amedeochiefa@gmail.com>
2025-01-07 14:25:09 +0000

    Correct yaml extension in eddie's script

--- scripts/eddie/run_fit.sh
+++ scripts/eddie/run_fit.sh
@@ -15,9 +15,9 @@ runcard_name=$(basename "$runcard")
 
 qsubpath=/exports/applications/gridengine/ge-8.6.5/bin/lx-amd64/qsub
 
-FIT_LOG_FOLDER=logs/logs_${runcard_name%".yml"}
+FIT_LOG_FOLDER=logs/logs_${runcard_name%".yaml"}
 FIT_LOG_FOLDER=$FIT_LOG_FOLDER/fit
 mkdir -p $FIT_LOG_FOLDER
 
-$qsubpath -N "MontBlanc_"${runcard_name%".yml"} -e $FIT_LOG_FOLDER -o $FIT_LOG_FOLDER -t 1-$nrep -l h_vmem=2500M -pe sharedmem 4 optimize.sh $runcard $fit_folder
+$qsubpath -N "MontBlanc_"${runcard_name%".yaml"} -e $FIT_LOG_FOLDER -o $FIT_LOG_FOLDER -t 1-$nrep -l h_vmem=2500M -pe sharedmem 4 optimize.sh $runcard $fit_folder
 
