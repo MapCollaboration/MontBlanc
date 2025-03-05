fbae61ea9323696cc9585732e1668afca7a95404
Canzian <canzian21@gmail.com>
2025-03-05 09:47:22 +0100

    Modified predictionshandler

--- src/predictions/predictionshandler.cc
+++ src/predictions/predictionshandler.cc
@@ -456,11 +456,18 @@ namespace MontBlanc
             TabSemiIncQIntegrand = std::unique_ptr<apfel::TabulateObject<apfel::Set<apfel::Operator>>>
                                    (new apfel::TabulateObject<apfel::Set<apfel::Operator>> {Nj, 50, 0.9 * Qmin, 1.1 * Qmax, 3, _Thresholds});
             // Push back multiplicities
+            if (YAML::Node parameter = config["observable"]) {
+              if (_bins[i].IntQ)
+              _FKt.push_back(apfel::Set<apfel::Operator> {pref * TabSemiIncQIntegrand->Integrate(Qmin, Qmax)});
+            else
+              _FKt.push_back(apfel::Set<apfel::Operator> {pref * TabSemiIncQIntegrand->Evaluate(_bins[i].Qav)});
+            }
+            else{
             if (_bins[i].IntQ)
               _FKt.push_back(apfel::Set<apfel::Operator> {pref * TabSemiIncQIntegrand->Integrate(Qmin, Qmax) / TabIncQIntegrand->Integrate(Qmin, Qmax)});
             else
               _FKt.push_back(apfel::Set<apfel::Operator> {pref * TabSemiIncQIntegrand->Evaluate(_bins[i].Qav) / TabIncQIntegrand->Evaluate(_bins[i].Qav)});
-
+            }
             xl = _bins[i].xmin;
             xu = _bins[i].xmax;
             xc = _bins[i].xav;
