45bb103afd16fecf786da330eff953dff28e4e3c
Canzian <canzian21@gmail.com>
2025-03-06 17:21:22 +0100

    changes in pred_V2

--- run/Predictions_SIDIS_V2.cc
+++ run/Predictions_SIDIS_V2.cc
@@ -107,9 +107,9 @@ int main(int argc, char *argv[])
    
   for (auto const& ds : config["Data"]["sets"])
        {
-        if(//ds["name"].as<std::string>().find("COMPASS") != std::string::npos || 
-              //ds["name"].as<std::string>().find("HERMES") != std::string::npos|| 
-              ds["name"].as<std::string>().find("EIC") != std::string::npos)
+        if(ds["name"].as<std::string>().find("COMPASS") != std::string::npos || 
+              ds["name"].as<std::string>().find("HERMES") != std::string::npos||)
+              //ds["name"].as<std::string>().find("EIC") != std::string::npos)
           {
             //Create folders
             std::string s =  ds["file"].as<std::string>();
@@ -135,9 +135,9 @@ int main(int argc, char *argv[])
   std::vector<std::pair<NangaParbat::DataHandler*, NangaParbat::ConvolutionTable*>> DSVect;
   for (auto const& ds : config["Data"]["sets"])
     {
-      if(//ds["name"].as<std::string>().find("COMPASS") != std::string::npos || 
-             // ds["name"].as<std::string>().find("HERMES") != std::string::npos
-             ds["name"].as<std::string>().find("EIC") != std::string::npos
+      if(ds["name"].as<std::string>().find("COMPASS") != std::string::npos || 
+             ds["name"].as<std::string>().find("HERMES") != std::string::npos
+             //ds["name"].as<std::string>().find("EIC") != std::string::npos
              )
        {
          std::cout << "#Experiment = " << ds["name"].as<std::string>() << std::endl;
